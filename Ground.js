e5164a5efadd479678728498ddd478a025fe431f
Sanju-Priya <75870681+Sanju-Priya@users.noreply.github.com>
2020-12-27 13:29:16 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,21 @@
+class Ground{
+
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+
+        this.bodies=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.bodies);
+    }
+
+    display(){
+        var pos=this.bodies.position;
+        rectMode(CENTER);
+        fill("green");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+
+}
